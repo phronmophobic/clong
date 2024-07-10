baa2b5bb482ed9ba02a6bbc254b50019bdc5f854
Adrian Smith <smith.adriane@gmail.com>
2024-07-10 15:44:57 -0700

    remove unnecessary trace when compiling native image.

--- examples/libz/compile_native.sh
+++ examples/libz/compile_native.sh
@@ -4,7 +4,6 @@ set -x
 
 native-image --report-unsupported-elements-at-runtime \
              --features=clj_easy.graal_build_time.InitClojureClasses \
-             --trace-object-instantiation=org.objectweb.asm.Type \
              --no-fallback \
              -H:ReflectionConfigurationFiles=config/reflect-config.json \
              -H:JNIConfigurationFiles=config/jni-config.json \
